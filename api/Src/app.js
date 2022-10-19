28067575fa6fb8c716ef174e972004c0a9130517
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-10-18 23:51:48 -0300

    a

--- api/Src/app.js
+++ api/Src/app.js
@@ -24,6 +24,14 @@ require("./routes/login/passportConfig");
 //     })
 // );
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://despegue.vercel.app'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+
 app.use(
     cookieSession({
         name: "session",
@@ -41,13 +49,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(morgan("dev"));
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://despegue.vercel.app'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+
 
 // app.use(
 //   cors({
