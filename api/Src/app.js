dad6120045aa6460f033743405241c20161e29e7
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-10-19 14:37:04 -0300

    a

--- api/Src/app.js
+++ api/Src/app.js
@@ -27,8 +27,6 @@ require("./routes/login/passportConfig");
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
   });
