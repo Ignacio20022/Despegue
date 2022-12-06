37bc210fed640955c1adb68f92977341335f4e38
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-06 11:39:14 -0300

    rollback folder name

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-const app = require("./source/app.js");
-const db = require("./source/db.js");
+const app = require("./src/app.js");
+const db = require("./src/db.js");
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("listening on port 3001");
