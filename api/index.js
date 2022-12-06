af9cd6fe55b3f8b83a212d56d73d2e5d7b39ee7f
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-06 11:36:05 -0300

    change folder name

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-const app = require("./Src/app.js");
-const db = require("./Src/db.js");
+const app = require("./source/app.js");
+const db = require("./source/db.js");
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("listening on port 3001");
